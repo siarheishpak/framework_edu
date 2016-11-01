4506e45f0575a4d1b843c3c8fd04775bec1a9caa
siarheishpak <shpaksv@gmail.com>
2016-10-31 21:21:25 -0400

    add rfaRunner and rfaUtils files
    Task 2: my own code

--- rfaRunner.py
+++ rfaRunner.py
@@ -1,27 +1,87 @@
 '''
 Created on Oct 19, 2016
 
-
-@author: sashaalexander
-@author: team X
+@author: Siarhei Shpak
 '''
-from rfaUtils import getLog,qaPrint
+from rfaUtils import getLog, qaPrint, getLocalEnv, getTestCases
 
-import sys
+import sys, optparse
+
+# path to the file with the description of environment variables
+ENV_FILE = "local.properties"
+
+# get the dictionary of the environment variables
+env_dict = getLocalEnv(ENV_FILE)
+# exit if file with environment variables wasn't found
+if env_dict == -1:
+    sys.exit("Failed to load environment variables")  
 
 # get the log file handle
-log = getLog()
-
+log = getLog(env_dict['log_dir'])
 # exit if log creation failed
 if log == -1:
-    sys.exit("Unable to create log file")
+    sys.exit("Failed to open or create log file")
+    
+qaPrint(log, 'Running %s script' % sys.argv[0])    
 
-message = "It is working, right?"
 
-# call qaPrint to print a message with timestamp and write it to the log file
-qaPrint(log, message)
-qaPrint(log, "Me like what me see")
+###################################
+# Process command-line arguments
+###################################
 
-# close the log file if it open
+# Create array of case insensitive arguments on the base of sys.argv[1:]
+case_insensitive_sys_args = []
+for arguments in sys.argv[1:]:
+    case_insensitive_sys_args.append(arguments.lower())
+
+# Create OptionParser instance
+parser = optparse.OptionParser(usage='Usage: %s [option]' % sys.argv[0])
+# Specify possible options
+parser.add_option('--testrun', action="store", dest="testrun", type="int",
+                   help="id number of running test", metavar="Test_ID")
+
+# Process command-line args
+try:
+    # Read command line args from case_insensitive_sys_args
+    opts, args = parser.parse_args(case_insensitive_sys_args)
+except (BaseException, Exception) as e:
+    qaPrint(log, 'Wrong arguments. Use --help for details.')
+    sys.exit()
+
+# Save value of 'testrun' option as trid      
+try:
+    if opts.testrun >= 0 and opts.testrun <= 10000:
+        trid = opts.testrun
+    else:
+        qaPrint(log, 'value of --testrun option should be in the range of [0;10000].')
+        sys.exit()
+except Exception as e:
+    qaPrint(log, str(e))
+    sys.exit()          
+
+
+###################################
+# Loading test cases
+###################################
+
+qaPrint(log, 'Loading test scenario_id=%s' % trid)
+# get the dictionary of test cases from test scenario id = 'trid'
+test_case = getTestCases(trid)
+# exit if file with test cases wasn't found
+if test_case == -1:
+    qaPrint(log, 'Failed to open file with test cases')
+    sys.exit()
+else:
+    qaPrint(log, 'Scenario_id=%s successfully loaded' % trid)    
+
+qaPrint(log, str(test_case)) # STRING SHOULD BE REMOVED LATER
+
+
+###################################
+# Closing log file
+###################################
+
 if not log.closed:
+    qaPrint(log, 'Closing log')
     log.close()
+                
--- rfaUtils.py
+++ rfaUtils.py
@@ -1,29 +1,32 @@
 '''
 Created on Oct 19, 2016
 
-@author: sashaalexander
-@author: team X
+@author: Siarhei Shpak
 '''
 from datetime import datetime
-import os
+import os , sys
 
+test_case_keys_list = ['tcid', 'rest_URL', 'HTTP_method', 'HTTP_RC_desired', 'param_list']
 
-def getLog():
+def getLog(log_dir_name):
     """
     Creates 'logs' directory, if it doesn't exist,
     creates or opens a log file in 'logs' directory.
     """
-    # assign a current working directory + '/logs' to log_dir variable (platform independent)
-    log_dir = os.path.join(os.getcwd(), "logs")
-    # or --> script directory: log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
-    # or --> user directory: log_dir = os.path.join(os.path.expanduser("~"), "logs")
-
+    # assign a current working directory + log_dir_name to log_dir variable (platform independent)
+    log_dir = os.path.join(os.getcwd(), log_dir_name)
+    # or --> script directory: log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), log_dir_name)
+    # or --> user directory: log_dir = os.path.join(os.path.expanduser("~"), log_dir_name)
+    
+    # get program name 
+    program_name = str(sys.argv[0]).split('/')[len(str(sys.argv[0]).split('/')) - 1]
+    
     try:
         # if logs directory(!) doesn't exist, create it
         if not os.path.isdir(log_dir):
             os.makedirs(log_dir)
         # open log file with prefix and timestamp (platform independent) in Append mode
-        log = open(os.path.join(log_dir, "rfaRunner_" + getCurTime("%Y%m%d_%H-%M") + ".log"), "a")
+        log = open(os.path.join(log_dir, program_name + "_" + getCurTime("%Y%m%d_%H-%M") + ".log"), "a")
         return log
     except (OSError, IOError):
         # return -1 in case of exception
@@ -37,7 +40,7 @@ def qaPrint(log, message):
     # current date and time as string + message. example: [Oct 25 01:52:33.000001] TC1 - Passed
     log_message = getCurTime("[%b %d %H:%M:%S.%f]") + " " + message
     # prints log_message
-    print log_message
+    print (log_message)
     # writes message to a log file
     log.write(log_message + "\n")
 
@@ -48,3 +51,68 @@ def getCurTime(date_time_format):
     """
     date_time = datetime.now().strftime(date_time_format)
     return date_time
+
+
+def getLocalEnv(env_file_path):
+    '''
+    Returns dictionary of environment settings
+    '''
+    # creates empty dictionary of environment settings
+    env_dict = {}
+    # open environment file for reading
+    try:
+        env_file = open(env_file_path, "r")
+        for line in env_file:
+            # save left part of the string divided by '=' as dictionary key
+            key = line.split('=')[0].rstrip() # rstrip() removes '\n'
+            # save left right of the string divided by '=' as dictionary value
+            value = line.split('=')[1].rstrip() # rstrip() removes '\n'
+            # add key-value pair to the dictionary
+            if key not in env_dict:
+                env_dict[key] = value
+            else:
+                return -1
+        env_file.close()
+        return env_dict
+    except (IOError, OSError):
+        # return -1 in case of error
+        return -1 
+    
+def getTestCases(test_run_id):
+    '''
+    Returns dictionary of test case settings
+    '''
+    # create empty dictionary of testcase settings
+    test_case_dict = {}
+    # open test case file for reading
+    try:
+        test_case_file = open(str(test_run_id) + '.txt', 'r')
+    except:
+        return -1
+    # saving content of the test case file into test_case_dict dictionary
+    for line in test_case_file:
+        # create empty inserted dictionary
+        test_case_insert_dict = {}
+        # saving content of the test case file into test_case_insert_dict dictionary
+        for i in range (0, len(test_case_keys_list)):
+            # use elements of 'test_case_keys_list' list as keys
+            key = test_case_keys_list[i]
+            # use content of test case file as dictionary values
+            value = line.split('|')[i].rstrip() # rstrip() removes '\n'
+            # insert key-value pair into 'test_case_insert_dict'
+            if key not in test_case_insert_dict:
+                test_case_insert_dict[key] = value
+            else:
+                return -1
+        # use number of test case as key for 'test_case_dict' dictionary
+        key = line.split('|')[0].rstrip() # rstrip() removes '\n'
+        # use 'test_case_insert_dict' as value for 'test_case_dict' dictionary
+        value = test_case_insert_dict
+        # insert key-value pair into 'test_case_dict'
+        if key not in test_case_dict:
+            test_case_dict[key] = value
+        else:
+            return -1
+    # close test case file
+    test_case_file.close()
+    return test_case_dict
